413a9d6d69da675bbf26ca83dbbd3f592cb784ec
mefdev <mohamedmeftah2018@gmail.com>
2023-07-28 04:08:39 +0100

    Do the second instruction section

--- client/src/assets/components/Hero.tsx
+++ client/src/assets/components/Hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import pattern1 from "../../../public/pattern1.png"
 import pattern2 from "../../../public/pattern2.png"
 import circle from "../../../public/circle.png"
-import circle1 from "../../../public/Ellipse 2522.png"
-import circle2 from "../../../public/Ellipse 2523.png"
-import circle3 from "../../../public/Ellipse 2524.png"
-import circle4 from "../../../public/Ellipse 2525.png"
-import circle5 from "../../../public/Ellipse 2526.png"
+import ellipse from "../../../public/elipses-background.png"
+import signupBtn from "../../../public/singupBtn.png"
+import instructionBtn from "../../../public/instruction-btn.png"
+import choseLang from "../../../public/choseLang.png"
+import arrow from "../../../public/arrow.png"
 
 export default function Hero(){
     return (
@@ -22,9 +22,34 @@ export default function Hero(){
                 <button className="btn">
                 Get Started
                 </button>
-                
-                   
+                <img className="ellipse" src={ellipse} alt="ellipses' background" />
+            </section>
+            <section className="instruction-container">
+                <h5>How it works</h5>
+                <h2>We make learning simple and straightforward</h2>
 
+                <div className="arrows-container">
+                <img className="firstArrow" src={arrow} alt="arrow image" />
+                <img className="secondArrow" src={arrow} alt="arrow image" />
+                </div>
+                <div className="main-instruction-container">
+                    <div>
+                        <img src={signupBtn} alt="singup icon" />
+                        <h6>Sign Up</h6>
+                        <p>Start by creating your account. It's quick, easy, and free! All you need is an email address,</p>
+                    </div>
+                    
+                    <div>
+                        <img src={choseLang} alt="Chose Language icon" />
+                        <h6>Choose Your Language</h6>
+                        <p>Decide  what to learn. You can always change your selection  in your account settings.</p>
+                    </div>
+                    <div>
+                        <img src={instructionBtn} alt="instruction icon" />
+                        <h6>Start Learning</h6>
+                        <p>Now the real fun begins! You'll start with a series of interactive lessons designed for your skill level.</p>
+                    </div>
+                </div>
             </section>
            
         </main>

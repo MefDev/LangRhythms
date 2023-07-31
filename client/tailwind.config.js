90b6ddda9d9dae606de6f4756ba1984f289590da
ZinoChan <devzinochan@gmail.com>
2023-07-31 17:48:34 +0100

    fix: add postcss to fix tailwindcss and add basic tailwind styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-100': '#525FE1',
+        'primary-200': '#FFCF59',
+        'secondary-100': '#FF60A8',
+        'secondary-300': '#A7CE4A'
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+      fontSize: {
+      lg: ['3rem', '1.3'],
+    }
+    },
   },
-  plugins: [],
+  variants: {},
+  plugins: [require('@tailwindcss/forms')],
 }

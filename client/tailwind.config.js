9ffbc3f4d3274bd2b3ba29790196aab647d076b6
ZinoChan <devzinochan@gmail.com>
2023-07-31 20:18:05 +0100

    styles: add more tailwind config and change body bg

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,9 @@ export default {
         'primary-100': '#525FE1',
         'primary-200': '#FFCF59',
         'secondary-100': '#FF60A8',
+        'secondary-200': '#F66742',
         'secondary-300': '#A7CE4A',
+        'secondary-400': '#9848FF'
       },
       fontFamily: {
         raleway: ['Raleway', 'sans-serif'],

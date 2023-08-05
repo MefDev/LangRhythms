030fcc9042709d0f039dbcc44b68971ddc311f08
ZinoChan <devzinochan@gmail.com>
2023-08-05 20:22:44 +0100

    Implement scroll top behavior on route change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoute from './routes/AppRoute'
+import ScrollToTop from './helpers/ScrollToTop'
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AppRoute />
     </BrowserRouter>
   )

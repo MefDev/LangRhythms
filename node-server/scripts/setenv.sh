32d23b334d95e1041273794392a7c7434e3415f8
ZinoChan <devzinochan@gmail.com>
2023-09-10 16:19:36 +0100

    Revert "Merge branch 'node-server'"
    This reverts commit aedd5490bf7dde072fe76fce27a8123a4e77c982, reversing
    changes made to 9ba12608ee5ca708a358603a0b12a41722748162.

--- node-server/scripts/setenv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# scripts/setenv.sh
-
-# Export test env vars
-export $(grep -v '^#' .env.test | xargs)
--- node-server/src/interfaces/responseStatus.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IResponseStatus {
-  status_code: number;
-  success: boolean;
-  data: any;
-  error: any;
-  timestamp: string | Date;
-}

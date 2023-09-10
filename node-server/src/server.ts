698955d62786e70eb9d7fb1d1a22fefdcf9ca32a
ZinoChan <devzinochan@gmail.com>
2023-09-10 10:42:49 +0100

    feat: implement auth controller and authRoute

--- node-server/src/server.ts
+++ node-server/src/server.ts
@@ -1,6 +1,12 @@
-import App from "@/app";
-import IndexRoute from "@/routes/index.route";
+import App from '@/app';
+import IndexRoute from '@/routes/index.route';
+import AuthRoute from './routes/auth.route';
+import AuthController from './controllers/auth.controller';
+import UserRepository from './data/repositories/userRepo';
 
-const server = new App([new IndexRoute()]);
+const server = new App([
+  new IndexRoute(),
+  new AuthRoute(new AuthController(new UserRepository())),
+]);
 
 server.listen();

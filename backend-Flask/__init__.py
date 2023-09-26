855dd5d08707dca969548889ae244d481bf73aa5
mefdev <mohamedmeftah2018@gmail.com>
2023-09-26 23:54:26 +0100

    The project structure for future devs

---
+++ backend-Flask/__init__.py
@@ -0,0 +1,14 @@
+# Backend-Flask/
+#     app/
+#         __init__.py
+#         server.py
+#         config.py
+#         models.py
+#     tests/
+#         __init__.py
+#         test_auth.py
+#         test_server.py
+#         datasets_test.py
+#         
+          
+#     

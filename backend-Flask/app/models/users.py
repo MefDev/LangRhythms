7813692a15737c6d356f8c2274b59d0c9cca6bb3
mefdev <mohamedmeftah2018@gmail.com>
2023-11-12 03:38:12 +0100

    Create a user table

--- backend-Flask/app/models/users.py
+++ backend-Flask/app/models/users.py
@@ -1,12 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
+from .import db
 from uuid import uuid4
-  
-db = SQLAlchemy()
+
   
 def get_uuid():
     return uuid4().hex
   
 class User(db.Model):
+    """Create a user table"""
     __tablename__ = "users"
     id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
     fullname = db.Column(db.String(150), unique=False)

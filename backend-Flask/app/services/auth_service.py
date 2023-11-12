bcf8717134c7965fb5ff2211ef76f9268fbfb078
mefdev <mohamedmeftah2018@gmail.com>
2023-11-12 22:25:36 +0100

    Check the password strength

--- backend-Flask/app/services/auth_service.py
+++ backend-Flask/app/services/auth_service.py
@@ -1,12 +1,31 @@
 
-from flask import jsonify
+from flask import jsonify, request
 from ..models.users import User
 from ..models import db
 from .api import email_validator_service
 from . import bcrypt
 import os
+from password_strength import PasswordPolicy 
 
 
+
+def valid_password(password):
+    """Check the strength of the password"""
+    policy = PasswordPolicy.from_names(
+    length=6,     # minimum length: 6 characters
+    uppercase=1,  # need min.  uppercase letters
+    numbers=1,    # need min.  digits
+    special=1,    # need min.  special characters
+    )
+
+    # Check the password against the policy
+    password_strength = policy.test(password)
+
+    if len(password_strength) == 0:
+        feedback = "Password is strong"
+    else:
+        feedback = "Password is weak"
+    return feedback
 
 def register_user(email, fullname, password):
     """Register a user to the DB"""
@@ -16,10 +35,16 @@ def register_user(email, fullname, password):
     
     secret_api_key = os.environ.get("ABSTRACT_API_KEY")
     is_email_valid = email_validator_service.check_email_isvalid(secret_api_key, email)
+    
     if is_email_valid:
-        hashed_password = bcrypt.generate_password_hash(password)
+        
+        # check password strength
+        is_password_valid = valid_password(password)
+        if (is_password_valid == "Password is weak"):
+            return jsonify({"error": "Password is weak"}), 403
         
         # create a new user
+        hashed_password = bcrypt.generate_password_hash(password)
         new_user = User(fullname=fullname, email=email,
                         password=hashed_password)
    
@@ -35,9 +60,4 @@ def register_user(email, fullname, password):
             }), 200
     else:
         return jsonify({"email": "The email is not valid"}), 409
-
-
-
-
-
-
+    

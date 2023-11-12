1335f0710bada69aca02d0963de4a69567ec4aff
mefdev <mohamedmeftah2018@gmail.com>
2023-11-13 00:28:51 +0100

    Handle the guide Get response

--- backend-Flask/app/blueprints/docs.py
+++ backend-Flask/app/blueprints/docs.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
 
-
 docs_blueprint = Blueprint("docs", __name__)
+
 
 @docs_blueprint.route("/")
 def guide():
+        """Handle the guide for docs"""
         return render_template("docs/guide.html")
+

1b221e1cbd7add95d2b53bda524be7b8ce5043db
mefdev <mohamedmeftah2018@gmail.com>
2023-11-16 00:07:29 +0100

    Invalidate and blacklist invalid tokens

--- backend-Flask/app/models/users.py
+++ backend-Flask/app/models/users.py
@@ -13,3 +13,10 @@ class User(db.Model):
     fullname = db.Column(db.String(150), unique=False)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.Text, nullable=False)
+    invalidated_tokens = db.Column(db.Text)
+
+    def invalidate_token(self, token_to_invalidate):
+        """Invalidate access_tokens"""
+        if token_to_invalidate not in self.invalidated_tokens:
+            self.invalidated_tokens = token_to_invalidate
+            db.session.commit()

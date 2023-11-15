44d8d53c7a94be7b4b61b9c1deb32afb89a4f2da
mefdev <mohamedmeftah2018@gmail.com>
2023-11-16 00:08:45 +0100

    Restrict access to certain routes

--- backend-Flask/app/blueprints/docs.py
+++ backend-Flask/app/blueprints/docs.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, render_template
+from flask_jwt_extended import jwt_required
+from ..services.auth_service import check_invalid_tokens
 
 docs_blueprint = Blueprint("docs", __name__)
 
 
 @docs_blueprint.route("/")
+@jwt_required()
 def guide():
         """Handle the guide for docs"""
         return render_template("docs/guide.html")
+
+@docs_blueprint.before_request
+@jwt_required()
+def check_blacklist():
+        response = check_invalid_tokens()
+        return response
 

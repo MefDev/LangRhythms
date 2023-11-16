d641f12aa0a97d151a4c928d8d6a0147f75c5c9c
mefdev <mohamedmeftah2018@gmail.com>
2023-11-16 01:47:17 +0100

    Save the information from 3rd api to DB

--- backend-Flask/app/services/api/auth_google_service.py
+++ backend-Flask/app/services/api/auth_google_service.py
@@ -7,23 +7,29 @@ import pathlib
 from google_auth_oauthlib.flow import Flow
 import google.auth.transport.requests
 from google.oauth2 import id_token
-from flask_jwt_extended import create_access_token
+
+from flask_jwt_extended import create_access_token, create_refresh_token, set_refresh_cookies, set_access_cookies
+from . import db, User, bcrypt
 
 
 def get_flow():
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+    # to allow Http traffic for local dev
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+    client_secrets_file = os.path.join(
+        pathlib.Path(__file__).parent, "client_secret.json")
 
     flow = Flow.from_client_secrets_file(
         client_secrets_file=client_secrets_file,
-        scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+        scopes=["https://www.googleapis.com/auth/userinfo.profile",
+                "https://www.googleapis.com/auth/userinfo.email", "openid"],
         redirect_uri="http://127.0.0.1:5000/api/google/callback"
     )
     return flow
 
 
 flow = get_flow()
+
 
 def google_user_login():
     """Authenticate the user with google 3rd paty api"""
@@ -32,18 +38,20 @@ def google_user_login():
     session["state"] = state
     return authorization_url
 
+
 def handle_google_login():
     """handle the core logic"""
-    
+
     GOOGLE_CLIENT_ID = os.environ.get("CLIENT_ID")
     flow.fetch_token(authorization_response=request.url)
     if not session["state"] == request.args["state"]:
         abort(500)  # State does not match!
-    
+
     credentials = flow.credentials
     request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    token_request = google.auth.transport.requests.Request(
+        session=cached_session)
 
     id_info = id_token.verify_oauth2_token(
         id_token=credentials.id_token,
@@ -63,20 +71,36 @@ def handle_google_login():
         "google_id": session["google_id"]
     }
     if session["google_id"] and session["name"] and session["email"]:
-            # Create a JWT access token
-            access_token = create_access_token(identity=user_credentials)
-            user_credentials["access_token"] = access_token
-            
-            # return a successful response with the populated object
-            return jsonify(user_credentials), 200
-    
+
+        # create an access token and refresh token
+        access_token = create_access_token(identity=session["email"])
+        refresh_token = create_refresh_token(identity=session["email"])
+
+        # Create a JWT access token
+        user_credentials["access_token"] = access_token
+
+        # retrieve email and fullname from 3rd party api google Auth
+        email = id_info.get("email")
+        fullname = id_info.get("name")
+
+        user_exists = User.query.filter_by(email=email).first()
+        # check if email not exist in our database
+        if not user_exists:
+            # create a new user
+            hashed_password = bcrypt.generate_password_hash("google_auth")
+            new_user = User(fullname=fullname, email=email,
+                            password=hashed_password, invalidated_tokens="")
+            #  Add the new user to the database
+            db.session.add(new_user)
+            db.session.commit()
+
+        # set the JWT cookies in the response
+        resp = jsonify({'login': True})
+        set_access_cookies(resp, access_token)
+        set_refresh_cookies(resp, refresh_token)
+
+        # return a successful response with the populated object
+        return jsonify(user_credentials), 200
+
     # if error occurs return an error response with corresponding error status code
     return jsonify({"error": 'the credentials are uncorrect'}), 401
-
-
-
-
-
-
-
-

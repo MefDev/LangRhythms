aa62ebb8eb44cb4a8e2e086927619904e31da02c
mefdev <mohamedmeftah2018@gmail.com>
2023-11-26 00:45:46 +0100

    all

--- backend-Flask/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-#     tests/
-#         __init__.py
-#         test_server.py
-#         test_base.py
--- backend-Flask/tests/constants.py
+++
@@ -1,5 +0,0 @@
-ABSOLUTE_PATH = f"Users/mohamed/Desktop"
-CONNECT_DB_FULL_URL = f"sqlite:////{ABSOLUTE_PATH}/LangRhythms/instance/flaskdb.db"
-
-ABSTRACT_API_URL = "https://emailvalidation.abstractapi.com/v1/"
-BASE_URL = "http://127.0.0.1:5000/"

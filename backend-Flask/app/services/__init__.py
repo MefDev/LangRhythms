b89beeba84d7837634692e50820af487b96472c7
mefdev <mohamedmeftah2018@gmail.com>
2023-11-25 01:31:10 +0100

    add comments

--- backend-Flask/app/services/__init__.py
+++ backend-Flask/app/services/__init__.py
@@ -2,5 +2,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from ..models import db
 from ..models.users import User
+
+# create global variable instances
 bcrypt = Bcrypt()
 jwt = JWTManager()

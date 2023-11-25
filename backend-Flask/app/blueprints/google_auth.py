2abff578297f39f87f79265e58039ff647bd3e77
mefdev <mohamedmeftah2018@gmail.com>
2023-11-25 01:24:51 +0100

    add explicit service of each function

--- backend-Flask/app/blueprints/google_auth.py
+++ backend-Flask/app/blueprints/google_auth.py
@@ -1,17 +1,18 @@
+"""Handle google api authentification blueprints and routes"""
 from flask import Blueprint, redirect
-from ..services.api.auth_google_service import google_user_login, handle_google_login
+from . import auth_google_service 
 
 google_auth_blueprint = Blueprint('api/google', __name__)
 @google_auth_blueprint.route("auth/login")
 def redirect_to_google_page():
     """Redirect to google login page"""
-    authorization_url =  google_user_login()
+    authorization_url =  auth_google_service.google_user_login()
     return redirect(authorization_url)
 
 @google_auth_blueprint.route("/callback")
 def google_login():
     """handle the google login Get response"""
-    response = handle_google_login()
+    response = auth_google_service.handle_google_login()
     return response
 
 @google_auth_blueprint.errorhandler(401)
